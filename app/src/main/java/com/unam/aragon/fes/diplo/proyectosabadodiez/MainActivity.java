4765e4330297242b316a82d6bec9a3ecccca32a1
diplofesa <edyironfist@gmail.com>
2018-02-10 13:45:24 -0600

    nueva actualziacion

--- app/src/main/java/com/unam/aragon/fes/diplo/proyectosabadodiez/MainActivity.java
+++ app/src/main/java/com/unam/aragon/fes/diplo/proyectosabadodiez/MainActivity.java
@@ -2,12 +2,27 @@ package com.unam.aragon.fes.diplo.proyectosabadodiez;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+    private Button miBoton;
+    private TextView textoVista;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        miBoton = findViewById(R.id.button);
+        textoVista=findViewById(R.id.textoVista);
+
+        miBoton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+               textoVista.setText("Hola GitHub");
+
+            }
+        });
     }
 }
